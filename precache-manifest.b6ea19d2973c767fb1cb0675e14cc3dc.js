50f388176452d2dcddbb34d1dfc0fffb48cd9b7a
Nitro <nitro0@naver.com>
2020-08-24 10:46:17 +0900

    Updates

---
+++ precache-manifest.b6ea19d2973c767fb1cb0675e14cc3dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68311e5bdcc6d9c89be6c969c965b02c",
+    "url": "/Cafeteria/index.html"
+  },
+  {
+    "revision": "ca9f2df98519b52ea05d",
+    "url": "/Cafeteria/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "77643daee702cb834edd",
+    "url": "/Cafeteria/static/css/main.704f5f81.chunk.css"
+  },
+  {
+    "revision": "ca9f2df98519b52ea05d",
+    "url": "/Cafeteria/static/js/2.bc3b7f5d.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Cafeteria/static/js/2.bc3b7f5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77643daee702cb834edd",
+    "url": "/Cafeteria/static/js/main.e9307ee7.chunk.js"
+  },
+  {
+    "revision": "83617c2cfd4a25f840da",
+    "url": "/Cafeteria/static/js/runtime-main.ac6ce7fd.js"
+  }
+]);
